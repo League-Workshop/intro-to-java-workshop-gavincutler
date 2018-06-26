4d159f2357ec49abb2bc0185ea817ab9002db1f5
gavincutler <league@WTS20.attlocal.net>
2018-06-26 14:58:08 -0700

    day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -7,6 +7,26 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
+		Robot r2d2 = new Robot();
+		r2d2.penDown();
+		r2d2.setSpeed(200);
+		
+		for(int i=0; i<4; i++) {
+			r2d2.move(250);
+			r2d2.turn(90);
+		}
+		r2d2.penUp();
+		r2d2.turn(180);
+		r2d2.move(100);
+		
+		r2d2.sparkle();
+		r2d2.penDown();
+		
+		for(int i=0; i<4; i++) {
+			r2d2.move(250);
+			r2d2.turn(90);
+		}
+		
 		// START HERE
 	
 		
